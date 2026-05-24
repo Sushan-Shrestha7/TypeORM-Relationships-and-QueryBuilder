3750fba7e53a1753e0d176352fd0265992536e43
Sushan Shrestha <sushanshrestha.7123@gmail.com>
2026-05-24 23:55:10 +0545

    swagger import

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,48 +11,61 @@ import { AppService } from './app.service';
 import { Student } from './Student';
 import { Assignment } from './assignment';
 import { Address } from './addresss';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Post('/savestudent')
+  @ApiOperation({ summary: 'Create a new student' })
   async createstudent(@Body() dto: Student) {
     return await this.appService.savestudent(dto);
   }
   @Post(':id/assignment')
+  @ApiOperation({ summary: 'Create a new assignment' })
   async createAssignment(@Param('id') id: number, @Body() dto: Assignment) {
     return await this.appService.createAssignment(id, dto);
   }
   @Post(':id/address')
+  @ApiOperation({ summary: 'Create a new address' })
   async createAddress(@Param('id') id: number, @Body() dto: Address) {
     return await this.appService.createAddress(id, dto);
   }
   @Get('/students')
+  @ApiOperation({ summary: 'Get all students' })
   async getallstudents() {
     return await this.appService.getAllStudents();
   }
   @Get('/address/:id')
+  @ApiOperation({ summary: 'Get address by ID' })
   async getAddressById(@Param('id') id: number) {
     return await this.appService.getAddressById(id);
   }
   @Get('/assignments/:id')
+  @ApiOperation({ summary: 'Get assignment by ID' })
   async getAssignmentById(@Param('id') id: number) {
     return await this.appService.getassignmentsbyid(id);
   }
   @Put('/updateassignment/:id')
-  async updateassignment(id: number, dto: Assignment) {
-    return this.appService.updateassignment(id, dto);
+  @ApiOperation({
+    summary: 'Update an existing assignment',
+  })
+  async updateassignment(@Param('id') id: number, @Body() dto: Assignment) {
+    return await this.appService.updateassignment(id, dto);
   }
   @Put('/updateaddress/:id')
-  async updateaddress(id: number, @Body() dto: Address) {
+  @ApiOperation({ summary: 'Update an existing address' })
+  async updateaddress(@Param('id') id: number, @Body() dto: Address) {
     return await this.appService.updateaddress(id, dto);
   }
   @Delete('/deletestudent/:id')
+  @ApiOperation({ summary: 'Delete a student' })
   async deletestudent(@Param('id') id: number) {
     return await this.appService.deletestudent(id);
   }
   @Get('/students/:id')
-  getStudent(@Param('id') id: number) {
-    return this.appService.getStudent(+id);
+  @ApiOperation({ summary: 'Get student by ID' })
+  async getStudent(@Param('id') id: number) {
+    return await this.appService.getStudent(+id);
   }
 }
--- src/assignment.ts
+++ src/assignment.ts
@@ -1,14 +1,30 @@
 import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Student } from './Student';
+import { ApiProperty } from 'node_modules/@nestjs/swagger/dist/decorators/api-property.decorator';
+import { IsEmpty, IsOptional } from 'class-validator';
 @Entity('assignments')
 export class Assignment {
   @PrimaryGeneratedColumn()
   id: number;
   @Column()
+  @ApiProperty({
+    example: 'Math Homework',
+    description: 'The title of the assignment',
+  })
   title: string;
   @Column()
+  @IsOptional()
+  @IsEmpty()
+  @ApiProperty({
+    example: 'Solve the following problems',
+    description: 'The description of the assignment',
+  })
   description: string;
   @Column()
+  @ApiProperty({
+    example: false,
+    description: 'The completion status of the assignment',
+  })
   Completed: boolean;
   @ManyToMany(() => Student, (student) => student.assignments)
   student: Student[];
